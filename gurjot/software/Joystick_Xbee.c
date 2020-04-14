11919b6b935c35e0e2169d68772abb33c8bb4914
gurjot302 <49761600+gurjot302@users.noreply.github.com>
2020-04-14 01:21:45 -0400

    Add files via upload

---
+++ gurjot/software/Joystick_Xbee.c
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h> 
+
+
+
+int x = 0;
+
+SoftwareSerial mySerial(10, 11);
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+ 
+ 
+  pinMode(A0,INPUT); // X
+
+
+  Serial.println("XBEE test");
+
+
+}
+
+void loop() {
+  x = analogRead(A0);
+
+ 
+  mySerial.println(x);
+  Serial.println("Data Sent to XBEE");
+  Serial.println(x);
+
+  delay(200); 
+
+ 
+
+}
